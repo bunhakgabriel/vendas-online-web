d782398d5886aced810b160396b348763fd61e76
Gabriel Bunhak <bunhakgabriel@gmail.com>
2024-01-25 23:42:03 -0300

    insert antd and style-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 
+import { Button } from "antd";
 import { useState } from "react";
+import styled from "styled-components";
 
 import viteLogo from "/vite.svg";
 
@@ -10,7 +12,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <EstriolDiv isBlue={count === 4} className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,9 +23,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -31,8 +33,26 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <TomatoButton>Button Styled</TomatoButton>
+    </EstriolDiv>
   );
 }
+
+interface PsDiv {
+  isBlue?: boolean;
+}
+
+const EstriolDiv = styled.div<PsDiv>`
+  color: ${(props) => (props.isBlue ? "blue" : "pink")};
+`;
+
+const TomatoButton = styled.button`
+  color: #bf4f74;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid black;
+  border-radius: 20px;
+`;
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import React from "react";
+import React from "react"; //A partir do react 17 nós não precisamos mais importar o react, essa linha de código não necessariamente precisa ser escrita
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
